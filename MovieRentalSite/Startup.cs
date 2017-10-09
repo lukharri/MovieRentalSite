653341421108cbe2c19e72c5a95afd6de34787bb
Cori <Cori@CORI>
2017-10-09 12:58:00 -0500

    Add project files.

---
+++ MovieRentalSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRentalSite.Startup))]
+namespace MovieRentalSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
