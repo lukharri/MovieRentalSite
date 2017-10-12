5a75a8c79d41b8a600a20df42cd43c1cacbfbb13
Luke Harris <luke.e.harris@gmail.com>
2017-10-12 12:02:24 -0500

    Convert JSON object properties from PascalCase to camelCase

--- MovieRentalSite/App_Start/WebApiConfig.cs
+++ MovieRentalSite/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,6 +11,15 @@ namespace MovieRentalSite
     {
         public static void Register(HttpConfiguration config)
         {
+            /*
+             * Properties of JSON objects returned from calling API are named with Pascal case
+             * Consuming these objects in JavaScript may cause problems
+             * Convert to camelCase to prevent any problems 
+             */
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
