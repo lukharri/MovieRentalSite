8c8eb48007f44531670cbccbf4aec7995da27904
Luke Harris <luke.e.harris@gmail.com>
2017-10-12 16:30:23 -0500

    Added javascript function to delete a customer. Styled dialog box w/bootbox

--- MovieRentalSite/App_Start/MappingProfile.cs
+++ MovieRentalSite/App_Start/MappingProfile.cs
@@ -14,8 +14,11 @@ namespace MovieRentalSite.App_Start
         {
             // calling createMap - automapper uses reflection to scan these types, finds
             // their properties and maps them based on their name
-            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>();  
         }
     }
 }
