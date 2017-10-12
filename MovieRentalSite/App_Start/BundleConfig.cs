4ec7f3c181f63c1a2adb59796f8f5c7e42619f39
Luke Harris <luke.e.harris@gmail.com>
2017-10-12 17:24:20 -0500

    Installed bootstrap dataTables and added it to the customers table

--- MovieRentalSite/App_Start/BundleConfig.cs
+++ MovieRentalSite/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace MovieRentalSite
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/Datatables/jquery.dataTables.js",
+                        "~/Scripts/Datatables/dataTables.bootstrap.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,12 +25,9 @@ namespace MovieRentalSite
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/Datatables/css/dataTables.bootstrap.css",
                       "~/Content/Site.css"));
         }
     }
